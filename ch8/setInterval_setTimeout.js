2d4d0403d8def80d574403d32493327fe0746650
afiya <khanafiya109@gmail.com>
2024-09-26 02:09:24 +0530

    new

---
+++ ch8/setInterval_setTimeout.js
@@ -0,0 +1,30 @@
+
+        // synatx of set_timeout and set_interval is same 
+
+        // setTimeout
+        // document.write("hello")
+        // let a = setTimeout(function () {
+        //     alert("i am inside of set timeout")    // will get displayed after 5 sec..(time mentioned after comma)
+        // }, 5000)
+        // console.log(a) //if we store it in a var a .it returns time id eg-1
+
+        // const sum = (a,b)=>{
+        //     console.log("sum is : ",a+b)
+        // }
+        // setTimeout(sum,1000,1,2)     //(func,time,parameters)....see result in console upon inspecting
+
+        // // clearTimeout
+        // let b = prompt("do u want to run the set time out?") // ans is y or n based on selection
+        // if (b == 'n') {
+        //     clearTimeout(a)   // if u give n  then alert of settimeout wont appear
+        // }
+
+        // //set interval
+        // setInterval(function(){
+        //     alert("i am set_interval") // will run after every 3 sec ...in an interval
+        // },3000)
+
+        //clearInterval()
+
+
+ 
